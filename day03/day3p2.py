1b204f66b16a5e546eaaaeb463185533bf8c78cd
Jurph <j.r.parsons@gmail.com>
2021-12-03 22:28:11 -0500

    I didn't say it was an ELEGANT solution.

--- day03/day3p2.py
+++ day03/day3p2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# Day 3, Part 1 of Advent of Code 2021
+# Day 3, Part 2 of Advent of Code 2021
 # Measure the most common bit(s) in a binary string 
-
-import binascii
 
 # Problem() is my class that ingests the day's input and structures it for easy computation
 class Problem():
@@ -12,7 +10,63 @@ class Problem():
             for line in file:
                 strings.append(line.rstrip())
         self.strings = strings
+        self.length = len(strings)
+        self.width = len(strings[0])
         return
+    
+    def mostpopular(self, offset=int):
+        sum = 0
+        for i in self.strings:
+            sum += int(i[offset])
+        popular = int(round(sum/self.length, 0))
+        # print("Sum was {} after {} rows; therefore return {}".format(sum, self.length, popular))
+        return popular
+
+def winfilter(diagnostics=list, placevalue=int):
+    j = placevalue
+    ones = []
+    zeroes = []
+    for i in diagnostics:
+        if i[j] == "1":
+            ones.append(i)
+        elif i[j] == "0":
+            zeroes.append(i)
+        else:
+            print("ERROR")
+        if len(ones) > len(zeroes):
+            winner = ones
+        elif len(zeroes) > len(ones):
+            winner = zeroes
+        elif len(ones) == 1:
+            winner = ones    
+        else:
+            winner = diagnostics
+    #print("Winner was {}".format(winner))
+    return winner
+
+def lossfilter(diagnostics=list, placevalue=int):
+    j = placevalue
+    ones = []
+    zeroes = []
+    winner = []
+    for i in diagnostics:
+        if i[j] == "1":
+            ones.append(i)
+        elif i[j] == "0":
+            zeroes.append(i)
+        else:
+            print("ERROR")
+        if len(ones) > len(zeroes):
+            winner = zeroes
+        elif len(zeroes) > len(ones):
+            winner = ones
+        elif len(zeroes) == 1:
+            winner = zeroes    
+        else:
+            winner = diagnostics
+    #print("Loser was {}".format(winner))
+    return winner
+
 
 def main():
     # Ingest and format the data
@@ -21,8 +75,8 @@ def main():
     # Set up state variables 
     gamma = ""
     epsilon = ""
-    rows = len(p.strings)
-    sums = [0]*len(p.strings[0])
+    rows = p.length
+    sums = [0]*p.width
 
     # Compute an answer
     for i in p.strings:
@@ -40,9 +94,28 @@ def main():
     gamma = int(str(gamma), 2)
     epsilon = int(str(epsilon), 2)
 
+    print("\n-=-= PART ONE =-=-\n")
     print("GAMMA   : {}".format(gamma))
     print("EPSILON : {}".format(epsilon))
     print("Product : {}".format(gamma * epsilon))
+    print("\n-=-= PART TWO =-=-\n")
+
+    oxygen = p.strings
+    carbon = p.strings
+    for q in range(p.width):
+        if len(oxygen) > 1:
+            oxygen = winfilter(oxygen, q)
+        if len(carbon) > 1:
+            carbon = lossfilter(carbon, q)
+    
+    print("Oxygen levels are        : {}".format(oxygen[0]))
+    print("CO2 levels are           : {}".format(carbon[0]))
+
+    oxygen = int(str(oxygen[0]), 2)
+    carbon = int(str(carbon[0]), 2)
+
+    print("Life Support Rating is   : {}".format(oxygen*carbon))
+
 
 if __name__ == "__main__":
     main()
