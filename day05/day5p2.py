62f765d25c260d1e40a79d4a0edcba4919c197d7
Jurph <j.r.parsons@gmail.com>
2021-12-05 11:51:21 -0500

    Day 5 part 2 works

---
+++ day05/day5p2.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python3
+# Day 5, Part 1 of Advent of Code 2021
+# Finding volcanic vents on the sea floor
+
+import sys
+import pathlib
+
+class Survey():
+    def __init__(self, filename):
+        entries = []
+        self.readings = []
+        with open(filename, "r") as file:
+            for line in file:
+                entries.append(line.rsplit())
+        for e in entries:
+            self.readings.append(Reading(e))
+        return
+
+class Reading():
+    def __init__(self, surveystring):
+        del surveystring[1]
+        startpoint = list(map(int, surveystring[0].split(",")))
+        endpoint = list(map(int, surveystring[1].split(",")))
+        self.pointlist = []
+        self.startx = int(startpoint[0])
+        self.starty = int(startpoint[1])
+        self.endx = int(endpoint[0])
+        self.endy = int(endpoint[1])
+        points = [[self.startx, self.endx], [self.starty, self.endy]]
+        self.isVertical = False
+        self.isHorizontal = False
+        self.isDiagonal = False
+        if self.startx == self.endx:
+            self.isVertical == True
+        elif self.starty == self.endy:
+            self.isHorizontal == True
+        else:
+            self.isDiagonal == True
+        self.fillPoints()
+        return
+
+    def fillPoints(self):
+        # print("\n-=-=-= Calculating points =-=-=-")
+        points = [[self.startx, self.starty], [self.endx, self.endy]]
+        dist = [self.endx - self.startx, self.endy - self.starty]
+        # print("Starting with {}".format(points))
+        # print("Computed distance vector of {}".format(dist))
+        if (dist[0] * dist[1]) != 0: # Must be diagonal
+            if dist[0] > dist[1]: # of the form (8, -8) : EndX larger, EndY smaller
+                # print("Diagonal (X, -Y)")
+                for d in range(1, dist[0]):
+                    points.append([self.startx + d, self.starty - d])
+            elif dist[0] < dist[1]: # of the form (-8, 8) : EndX smaller, EndY larger
+                # print("Diagonal (-X, Y)")
+                for d in range(1, dist[1]):
+                    points.append([self.startx - d, self.starty + d])
+            elif abs(dist[0]) == dist[0]: # of the form (8, 8) : End points both larger 
+                # print("Diagonal (X, Y)")
+                for d in range(1, abs(dist[0])):
+                    points.append([self.startx + d, self.starty + d])
+            else: # of the form (-8, -8) : End points both smaller
+                # print("Diagonal (-X, -Y)")
+                for d in range(1, abs(dist[0])):
+                    points.append([self.startx - d, self.starty - d])
+        elif (dist[1] > 0):
+            # print("Vertical")
+            for y in range(self.starty+1, self.endy):
+                points.append([self.startx, y])
+            # print("Made {} vertical points: {}".format(len(range(self.starty, self.endy)), points))
+        elif (dist[1] < 0):
+            # print("Vertical")
+            for y in range(self.endy+1, self.starty):
+                points.append([self.startx, y])
+            # print("Made {} vertical points: {}".format(len(range(self.starty, self.endy)), points))
+        elif (dist[0] > 0):
+            # print("Horizontal")
+            for x in range(self.startx+1, self.endx):
+                points.append([x, self.starty])
+            # print("Made {} horizontal points: {}".format(len(range(self.startx, self.endx)), points))
+        elif (dist[0] < 0):
+            # print("Horizontal")
+            for x in range(self.endx+1, self.startx):
+                points.append([x, self.starty])
+            # print("Made {} horizontal points: {}".format(len(range(self.startx, self.endx)), points))
+        else:
+            # print("Skipped??")
+            points = []
+            pass
+            # print("Made no points.")            
+        self.pointlist = points
+        print(points)
+        return
+        
+
+class SeaFloor():
+    def __init__(self, x=1000, y=1000):
+        self.easting = x
+        self.northing = y
+        self.ventchart = [[0 for _ in range(x)] for _ in range(y)]
+        return
+
+    def addvent(self, xypoint: list, vents=1):
+        x = xypoint[1]
+        y = xypoint[0]
+        self.ventchart[x][y] += vents
+        return
+
+    def countvents(self, dangerlevel=1):
+        dangerousvents = 0
+        for row in self.ventchart:
+            for entry in row:
+                if entry >= dangerlevel:
+                    dangerousvents += 1
+                else:
+                    pass
+        return dangerousvents
+
+    def print(self):
+        printstring = ""
+        for row in self.ventchart:
+            rowstring = "  "
+            for entry in row:
+                if entry == 0:
+                    rowstring += "."
+                else:
+                    rowstring += str(entry)
+            printstring += rowstring
+            printstring += "\n"
+        print(printstring)
+        return
+
+
+def main():
+    # Parse command-line args to see if a non-default file is specified
+    if len(sys.argv) > 1:
+        filename = pathlib.Path(sys.argv[1])
+    else:
+        filename = "C:\\Users\\Jurph\\Documents\\Python Scripts\\aoc2021\\day05\\test.txt"
+        print("No input file specified.")
+    print("...using {}".format(filename))
+    survey = Survey(filename)
+    f = SeaFloor()
+    for r in survey.readings:
+        for p in r.pointlist:
+            f.addvent(p)
+    print("DANGER SCAN: Found {} vents of Danger Level 2 or worse".format(f.countvents(2)))
+    # f.print()
+    return
+
+if __name__ == "__main__":
+    main()
