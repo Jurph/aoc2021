e9ba50c494827c65b57791c9a871261d31d5c5ad
Jurph <j.r.parsons@gmail.com>
2021-12-05 14:42:03 -0500

    Too much polish, honestly

--- day05/day5p2.py
+++ day05/day5p2.py
@@ -6,37 +6,31 @@ import sys
 import pathlib
 
 class Survey():
-    def __init__(self, filename):
+    def __init__(self, filename:str):
         entries = []
         self.readings = []
+        self.span = 0
         with open(filename, "r") as file:
             for line in file:
                 entries.append(line.rsplit())
         for e in entries:
             self.readings.append(Reading(e))
+        for r in self.readings:
+            self.span = max(self.span, r.span)
         return
 
 class Reading():
-    def __init__(self, surveystring):
-        del surveystring[1]
-        startpoint = list(map(int, surveystring[0].split(",")))
-        endpoint = list(map(int, surveystring[1].split(",")))
+    def __init__(self, surveystrings:list):
+        del surveystrings[1]        # Removes the "->" entry from the list
+        startpoint = list(map(int, surveystrings[0].split(",")))
+        endpoint = list(map(int, surveystrings[1].split(",")))
         self.pointlist = []
         self.startx = int(startpoint[0])
         self.starty = int(startpoint[1])
         self.endx = int(endpoint[0])
         self.endy = int(endpoint[1])
-        points = [[self.startx, self.endx], [self.starty, self.endy]]
-        self.isVertical = False
-        self.isHorizontal = False
-        self.isDiagonal = False
-        if self.startx == self.endx:
-            self.isVertical == True
-        elif self.starty == self.endy:
-            self.isHorizontal == True
-        else:
-            self.isDiagonal == True
         self.fillPoints()
+        self.span = max(self.startx, self.starty, self.endx, self.endy)
         return
 
     def fillPoints(self):
@@ -72,17 +66,16 @@ class Reading():
             points = []
             pass
         self.pointlist = points
-        print(points)
         return
         
 
 class SeaFloor():
-    def __init__(self, x=1000, y=1000):
-        self.easting = x
-        self.northing = y
-        self.ventchart = [[0 for _ in range(x)] for _ in range(y)]
+    def __init__(self, size=1000):
+        self.size = size
+        self.ventchart = [[0 for _ in range(size)] for _ in range(size)]
         return
 
+    # AddVent of Code 2021
     def addvent(self, xypoint: list, vents=1):
         x = xypoint[0]
         y = xypoint[1]
@@ -100,18 +93,21 @@ class SeaFloor():
                     pass
         return dangerousvents
 
-    def print(self):
-        printstring = ""
-        for row in self.ventchart:
-            rowstring = "  "
-            for entry in row:
-                if entry == 0:
-                    rowstring += "."
-                else:
-                    rowstring += str(entry)
-            printstring += rowstring
-            printstring += "\n"
-        print(printstring)
+    def print(self, maxsize=80):
+        if self.size > maxsize:
+            print("Sea Chart exceeds {0} x {0} - No Display".format(maxsize))
+        else:
+            printstring = ""
+            for row in self.ventchart:
+                rowstring = "  "
+                for entry in row:
+                    if entry == 0:
+                        rowstring += "."
+                    else:
+                        rowstring += str(entry)
+                printstring += rowstring
+                printstring += "\n"
+            print(printstring)
         return
 
 
@@ -122,18 +118,22 @@ def main():
     else:
         filename = "C:\\Users\\Jurph\\Documents\\Python Scripts\\aoc2021\\day05\\test.txt"
         print("No input file specified.")
-    print("...using {}".format(filename))
+    print("Using {}\n\n".format(filename))
 
     # Start solving the problem
     survey = Survey(filename)
-    f = SeaFloor()
+    f = SeaFloor(survey.span + 1)
     dangerlevel = 2
     for r in survey.readings:
         for p in r.pointlist:
             f.addvent(p)
-    print("DANGER SCAN: Found {} vents of Danger Level {} or worse".format(f.countvents(dangerlevel), dangerlevel))
-    if f.easting < 80:
-        f.print()
+
+    # Output the answers 
+    # For my inputs the correct answers are
+    #   test.txt  - 12      [prints a chart]
+    #   input.txt - 18442   [no chart print]
+    print("DANGER SCAN: Found {} vents of Danger Level {} or worse\n".format(f.countvents(dangerlevel), dangerlevel))
+    f.print()
     return
 
 if __name__ == "__main__":
