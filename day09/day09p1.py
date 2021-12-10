e6f36027e5f97a750a6be5810f950c161028f79d
Jurph <j.r.parsons@gmail.com>
2021-12-09 19:57:50 -0500

    Part 1 works - Part 2 looks like a stretch for me

--- day09/day09p1.py
+++ day09/day09p1.py
@@ -32,6 +32,7 @@ class Survey():
         for row in range(self.height):
             rowstring = ' '
             for col in range(self.width):
+                # TODO: add fancy ANSI highlighting of low points :) 
                 rowstring += str(self.grid[row][col])
             rowstring += '\n'
             printstring += rowstring
@@ -41,9 +42,31 @@ class Survey():
     def risklevel(squarevalue):
         return int(1+squarevalue)
 
-    def countLowPoints(self, grid):
-        
-        return
+    def countRiskScore(self):
+        risk = 0
+        for row in range(self.height):
+            for col in range(self.width):
+                neighbors = []
+                if row == 0:
+                    neighbors.append(self.grid[row + 1][col])
+                elif row == self.height-1:
+                    neighbors.append(self.grid[row - 1][col])
+                else:
+                    neighbors.append(self.grid[row + 1][col])
+                    neighbors.append(self.grid[row - 1][col])
+
+                if col == 0:
+                    neighbors.append(self.grid[row][col + 1])
+                elif col == self.width - 1:
+                    neighbors.append(self.grid[row][col - 1])
+                else:
+                    neighbors.append(self.grid[row][col + 1])
+                    neighbors.append(self.grid[row][col - 1])
+                print("Checking {} against value of {}".format(self.grid[row][col], sorted(neighbors)))
+                if self.grid[row][col] < sorted(neighbors)[0]:
+                    risk += self.grid[row][col] + 1
+                    print("Found low point at {},{}".format(row, col))
+        return risk
 
 def main():
     # Parse command-line args to see if a non-default file is specified
@@ -58,6 +81,8 @@ def main():
     p = Survey(filename)
     print(p.grid)
     p.print()
+    j = p.countRiskScore()
+    print(j)
 
     # Brute force the problem
     total = 0
